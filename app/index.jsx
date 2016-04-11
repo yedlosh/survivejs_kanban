fce20ebf17e1daf7a3a1b58bf2c116ce3e4c8547
Honza Jedlicka <yedlosh@gmail.com>
2016-04-11 16:16:02 +0200

    Add tutorial implementation

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+require('./main.css');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
+
+persist(alt, storage, 'app');
+
+ReactDOM.render(<App />, document.getElementById('app'));
